93a8af902e943ce3485e943be3c712a52d1fe0a1
yongbin999 <yongbinliang@umass.edu>
2015-11-22 15:02:09 -0500

    added more demos on loopback, but decide to abandon

--- Molecular_Admin_Client/demo_apps/loopback-getting-started-intermediate/server/boot/routes.js
+++ Molecular_Admin_Client/demo_apps/loopback-getting-started-intermediate/server/boot/routes.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
   // Install a "/ping" route that returns "pong"
-  app.get('/ping', function(req, res) {
-    res.send('pong');
+var router = app.loopback.Router();
+  router.get('/ping', function(req, res) {
+    res.send('pongaroo');
   });
+  app.use(router);
 };
