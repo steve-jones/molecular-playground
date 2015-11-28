cd91edb3a34fe3eee8dac610fcd636f9935f1ba4
jcalabro <jcalabro@umass.edu>
2015-11-28 16:44:25 -0500

    updates the Users sql file for reference

--- database/SQL/Users.sql
+++ database/SQL/Users.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Users (
 	id INT PRIMARY KEY NOT NULL,
 	first_name TEXT NOT NULL,
@@ -7,6 +8,8 @@ CREATE TABLE Users (
 	email TEXT NOT NULL,
 	role INT NOT NULL
 );
+
+
 
 CREATE OR REPLACE FUNCTION add_user(firstName text, lastName text, username text, password text, email text, role int)
 RETURNS int as $IDKey$
@@ -21,15 +24,13 @@ BEGIN
 	INSERT INTO Users VALUES (IDKey, firstName, lastName, username, password, email, role);
 	RETURN IDKey;
 END;
-/*this is still in progress*/
-/*CREATE OR REPLACE FUNCTION update_profile(firstName text, lastName text, username text, password text, email text, role int)
-RETURNS boolean as $updateSuccessful$
-DECLARE
-	updateSuccessful boolean;
-BEGIN
-	IF (text IS NOT NULL) THEN
-		INSERT INTO Users VALUES (text);	
-	END IF;
-	
-END;*/
 $IDKey$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION update_email(new_username text, new_email text)
+RETURNS void AS $$
+BEGIN
+    UPDATE users SET email=new_email WHERE username=new_username;
+END;
+$$ LANGUAGE plpgsql;
