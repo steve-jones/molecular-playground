534c698b29e75f0ba0bec2e2d02338e5f3c9a8f8
Freddy Nguyen <haiqnguyen318@gmail.com>
2015-11-29 00:02:35 -0500

    add comments to installation routes

--- devapp_v1/routes/installation_api.js
+++ devapp_v1/routes/installation_api.js
@@ -1,37 +1,39 @@
 var express = require('express');
 var router = express.Router();
+
+// object which contains DB access functions
 var model = require('../model/installtion_functions');
 
+// TODOs will be completed once DB API is ready.
 router.post('/create', function(req,res) {
 	var user = req.session.user;
-	if (!check(user)) {
-		res.redirect('/login');
-	}
+	if (!check(user)) res.redirect('/login');
 	else {
-
+		// TODO: create installation row in DB.
 	}
 });
 
-router.post('/remove', function(req,res) {
+router.post('/remove/:id', function(req,res) {
 	var user = req.session.user;
-	if (!check(user)) {
-		res.redirect('/login');
-	}
+	if (!check(user)) res.redirect('/login');
+	else if (!req.params.id) res.sendStatus(400);
 	else {
-
+		// TODO: delete installation row from DB.
 	}
 });
 
-router.post('/edit', function(req,res) {
+router.post('/update_local_admin/:id', function(req,res) {
 	var user = req.session.user;
-	if (!check(user)) {
-		res.redirect('/login');
-	}
+	if (!check(user)) res.redirect('/login');
+	else if (!req.params.id) res.sendStatus(400);
 	else {
-
+		// TODO: update local admin column by row ID in DB.
 	}
 });
 
+/////////
+// Utility Functions
+/////////
 function check(user) {
 	return user && user.role === 'global_admin';
 }
