a2e7f217bd33e218eff686b1528e10850eb75906
jcalabro <jcalabro@umass.edu>
2015-12-01 12:22:28 -0500

    Removes files not being merged into master

--- database/main.js
+++
@@ -1,9 +0,0 @@
-
-var db = require('./usersAPI.js');
-
-// db.createUser('test', 'test', 'jcalabro', 'password', 'ganderson@umass.edu', 2);
-db.updateRole('jcalabro', 3);
-
-
-console.log("Done");
-return;
