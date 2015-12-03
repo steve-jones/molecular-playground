d026a710061ef36fadf81c44b395ec6a95af59f1
Ethan Miller <ethan.w.miller@gmail.com>
2015-12-02 20:13:29 -0500

    restructured

--- Existing_Molecular_Playground/src/playground_app-web/db/migrate/20120811060300_create_installations_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateInstallationsUsers < ActiveRecord::Migration
-  def change
-  	create_table :installations_users, id: false do |t|
-  		t.integer :installation_id
-  		t.integer :user_id
-  	end
-  end
-end
--- Existing_Molecular_Playground/src/playground_app-web/db/migrate/20120816172618_change_description_to_text.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDescriptionToText < ActiveRecord::Migration
-  def up
-  	change_column :scenes, :description, :text
-  end
-
-  def down
-  	change_column :scenes, :description, :string
-  end
-end
