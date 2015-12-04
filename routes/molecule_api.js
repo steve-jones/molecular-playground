d12552095497143c83af1933c1e39b343ceab62a
Emma Hyde <emmajhyde@gmail.com>
2015-12-04 15:28:09 -0500

    Removed remove, update functionality
    Also removed success/fail states for simplicity. Waiting on defining
    removeRequest() pop functionality for database (unclear to me how to
    implement.)

--- routes/molecule_api.js
+++ routes/molecule_api.js
@@ -9,10 +9,7 @@ router.get('/', function(req, res) {
 	res.render('molecule_page', { userinfo   : user_obj});
 });
 
-			//NOTE: requests need to be classified into type of deletion for my routes to work...
-			// TODO: define 'success' conditional
 			// TODO: define removeRequest() pop function
-			// TODO: define retrieved_file.request_type(), returns request_type (upload, deletion, update)
 
 ////// UPLOAD ///////
 // Upload a molecule, pending approval
@@ -31,85 +28,42 @@ router.post('/uploadmolecule', function(req,res) {
 			if (!validGlobalAdmin(user)){
 				retrieved_file = (( RETRIEVED FILE PLACEHOLDER ));
 				addToPendingRequest : function (retrieved_file, upload_request); (( DB PLACEHOLDER ))
-					//Success or fail state
-				if ('success'){
-					res.redirect('back');
-					req.flash('upload_success_state', "Uploaded successfully. New molecule pending approval.");
-				}
-				else {
-					res.redirect('back');
-					req.flash('upload_fail_state', "Not uploaded successfully. Please try again.");
-
-				}
+				//Success or fail state
+				// if ('success'){
+				res.redirect('back');
+				req.flash('upload_success_state', "Uploaded. New molecule pending approval.");
+				// }
+				// else {
+				// 	res.redirect('back');
+				// 	req.flash('upload_fail_state', "Not uploaded successfully. Please try again.");
+				// }
 			}
 		//if it is a global admin, approve immediately
 			else approveNewMolecule : function(retrieved_file);
 		}
 });
 
+		/*
+		We found that, given enough time, we would classify requests into separate queues.
+		We would have a deletion queue, an update queue, and a new molecule queue.
+		We were able to write the backend code for both.
+		However, we were not able to alter the database given the amount of time.
+		If we were to implement them, we would have a request classifier that would sort
+		it into the correct queue, and then they could be approved or rejected by a global admin.
+		The following placeholders are where the routing code would go for these functions.
+		*/
+
 ////// DELETE ///////
 // Delete a molecule, pending approval
 // GLOBAL ADMIN request approves immediately
 /////////////////////
-
-router.post('/deletemolecule', function(req,res) {
-	var user = req.session.user;
-		//base user cases -- not a valid user
-		if (!validRole(user)) {
-			res.redirect('/login');
-	    	req.flash('invalid_role', "Invalid Permissions");
-	    }
-	    //if it is not a global admin but is some other user, go through approval process
-		else{
-			if (!validGlobalAdmin(user)) {
-				retrieved_file = (( RETRIEVED FILE PLACEHOLDER ));
-				addToPendingRequest : function (retrieved_file, deletion_request); (( DB PLACEHOLDER ))
-				if ('success'){
-					res.redirect('back');
-					req.flash('deletion_success_state', "Deletion requested. Pending approval.");
-				}
-				else {
-					res.redirect('back');
-					req.flash('deletion_fail_state', "Deletion request failed. Please try again.");
-
-				}
-			}
-		// if it is a global admin, approve immediately
-			else approveDeletion : function(retrieved_file);
-
-		}
-});
 
 ////// UPDATE ///////
 // Update a pre-existing molecule, pending approval
 // GLOBAL ADMIN request approves immediately
 /////////////////////
 
-router.post('/updatemolecule', function(req,res) {
-	var user = req.session.user;
-		//base user cases -- not a valid user
-		if (!validRole(user)) {
-			res.redirect('/login');
-	    	req.flash('invalid_role', "Invalid Permissions");
-	    }
-		else{
-			if (!validGlobalAdmin(user)) {
-				retrieved_file = (( RETRIEVED FILE PLACEHOLDER ));
-				addToPendingRequest : function (retrieved_file, update_request); (( DB PLACEHOLDER ))
-				if ('success'){
-					res.redirect('back');
-					req.flash('update_success_state', "Update requested, update uploaded successfully. Pending approval.");
-				}
-				else {
-					res.redirect('back');
-					req.flash('update_fail_state', "Update not uploaded successfully, no update requested. Please try again.");
-
-				}
-			}
-		// if it is a global admin, approve immediately
-			else approveUpdate : function(retrieved_file);
-		}
-});
+		/*END PLACEHOLDERS*/
 
 ////// APPROVE //////
 // Approves a selected request in collection of requests that are pending approval
@@ -119,40 +73,19 @@ router.post('/updatemolecule', function(req,res) {
 router.get('/approverequest', function(req,res) {
 	var user = req.session.user;
 		//base user cases -- not a valid user
-		if (!validGlobalAdmin(user)) {
+		if (!validRole(user) || !validGlobalAdmin(user)) {
 			res.redirect('/login');
 	    	req.flash('invalid_role', "Invalid permissions. Please log in to a global admin account.");
 	    }
 		else {
 			retrieved_file = (( RETRIEVED FILE PLACEHOLDER ));
-			retrieved_request_type = //retrieved_file.request_type();
-
-			switch (retrieved_request_type) {
-				case 'upload_request':
-					approveNewMolecule : function(retrieved_file);
-					res.redirect('back');
-					if ('success'){
-						req.flash('approval_success_state', "Approved new molecule.");
-					}
-					else req.flash('approval_fail_state', "Approval failure. Please refresh.");
-					break;
-				case 'deletion_request':
-					approveDeletion : function(retrieved_file);
-					res.redirect('back');
-					if ('success'){
-						req.flash('approval_success_state', "Approved deletion.");
-					}
-					else req.flash('approval_fail_state', "Approval failure. Please refresh.");
-					break;
-				case 'update_request':
-					approveUpdate : function(retrieved_file);
-					res.redirect('back');
-					if ('success'){
-						req.flash('approval_success_state', "Approved update.");
-					}
-					else req.flash('approval_fail_state', "Approval failure. Please refresh.");
-					break;
-			}
+			approveNewMolecule : function(retrieved_file);
+			removeRequest(); //placeholder
+			res.redirect('back');
+			// if ('success'){
+			req.flash('approval_success_state', "Approved new molecule.");
+			// }
+			// else req.flash('approval_fail_state', "Approval failure. Please refresh.");
 		}
 });
 
@@ -164,20 +97,19 @@ router.get('/approverequest', function(req,res) {
 router.get('/rejectrequest', function(req,res) {
 	var user = req.session.user;
 		//base user cases -- not a valid user
-		if (!validGlobalAdmin(user)) {
+		if (!validRole(user) || !validGlobalAdmin(user)) {
 			res.redirect('/login');
 	    	req.flash('invalid_role', "Invalid permissions. Please log in to a global admin account.");
 	    }
 		else {
 			retrieved_file = (( RETRIEVED FILE PLACEHOLDER ));
 			retrieved_request_type = //retrieved_file.request_type();
-
 			removeRequest(); // placeholder
 			res.redirect('back');
-			if ('success'){
-				req.flash('reject_success_state', "Rejected request.");
-			}
-			else req.flash('reject_fail_state', "Failed to reject request. Please refresh.");
+			// if ('success'){
+			req.flash('reject_success_state', "Rejected request.");
+			// }
+			// else req.flash('reject_fail_state', "Failed to reject request. Please refresh.");
 		}
 });
 
