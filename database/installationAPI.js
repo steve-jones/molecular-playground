1d4201aafb64de09e55b396e07ffdf76f4805df1
Philron William Hozier <phozier@umass.edu>
2015-12-06 15:43:51 -0500

    committing route handling logic for installation/create (form handling post requests). minor db fixes also to installationAPI

--- database/installationAPI.js
+++ database/installationAPI.js
@@ -9,54 +9,53 @@ module.exports = {
 		dbReader.executeFunction('get_installations', '', function(installations){
 			callback(installations);
 		});
-	}, 
+	},
 
-	addInstallation : function (city, country, school_affiliation, local_admin_id, GPS_location_x, GPS_location_y){
+	addInstallation : function (city, country, school_affiliation, local_admin_id, GPS_location_x, GPS_location_y, callback){
 		var date = new Date();
 		var day = date.getDate();
-		parameters = [city, country, school_affliation, local_admin_id, {}, true, day,
+		console.log("recieved contents: "+ city, country, school_affiliation, local_admin_id, GPS_location_x, GPS_location_y)
+		parameters = [city, country, school_affiliation, local_admin_id, '{}', true, day,
 		GPS_location_x, GPS_location_y];
-		dbReader.executeFunction('add_installation', parameters, function(){
+		dbReader.executeFunction('add_installation', parameters, function(err){
+			console.log(err);
+			callback(err);
 		});
-	} 
+	},
 
 	deleteInstallation : function(installation_id){
 		dbreader.executeFunction('delete_installation', installation_id, function(){
 		});
-	}
+	},
 
 	disableInstallation : function(installation_id){
 		dbreader.executeFunction('disable_installation', installation_id, function(){
 		});
-	}
+	},
 
 	getLocalDelegates : function(installation_id, callback){
 		dbreader.executeFunction('get_local_delegates', installation_id, function(allDelegates){
 			callback(allDelegates);
 		});
-	}
+	},
 
-	updateLocalAdmin : function(installation_id, new_local_admin_id){
-		parameters = [installation_id, new_local_admin_id]
-		dbreader.executeFunction('update_local_admin', parameters, function(){
-		});
-	}
+	//edit installation what to edit??
 
+	//this may require a callback in createUser otherwise it might not work
 	addLocalDelegate : function(installation_id, firstName, lastName, username, password, email, role){
-		dbUser.createUser(firstName, lastName, username, password, email, role, function(err){
-			dbUser.getUser(username,function(userData){
-				var userID = userData[0].id;
-				dbreader.executeFunction('add_local_delegate', userID, function(){
-				});
+		dbUser.createUser(firstName, lastName, username, password, email, role);
+		dbUser.getUser(username,function(userData){
+			var userID = userData[0].id;
+			dbreader.executeFunction('add_local_delegate', userID, function(){
 			});
 		});
-	}
+	},
 
 	removeLocalDelegate : function(installation_id, delegate_id, delegate_username){
 		parameters = [installation_id, delegate_id];
-		dbreader.executeFunction('remove_local_delegate', parameters, function(){	
+		dbreader.executeFunction('remove_local_delegate', parameters, function(){
 			dbUser.deleteUser(delegate_username);
 		});
 	}
 
-}	
+}
