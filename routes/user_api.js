189d188d80813ae0b37a54515411380888ce0a7b
Ethan Miller <ethan.w.miller@gmail.com>
2015-12-08 09:21:34 -0500

    password length verification

--- routes/user_api.js
+++ routes/user_api.js
@@ -23,11 +23,15 @@ router.post('/createuser', function(req,res) {
 			// call db function for create user
 			// Parameters: (String) firstName, (String) lastName, (String) username,
 			// (String) password, (String) email, (Number) role
-			// TODO: verify password and such
+			// TODO: verify password length and such
 			var firstName = req.body.firstName;
 			var lastName = req.body.lastName;
 			var username = req.body.username;
 			var password = req.body.password;
+			if(password.length < 6) {
+				console.log("Password isn't long enough.");
+				res.redirect('/login');
+			}
 			var email = req.body.email;
 			var role = req.body.role;
 				db.createUser(firstName, lastName, username, password, email, role, function(err){
