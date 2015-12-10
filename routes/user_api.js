013ab1bffd394d555fb765af022befc442580e3b
Ethan Miller <ethan.w.miller@gmail.com>
2015-12-09 20:55:49 -0500

    basic user routes seem done:

--- routes/user_api.js
+++ routes/user_api.js
@@ -14,26 +14,26 @@ router.get('/', function(req, res) {
 });
 
 router.get('/add', function(req, res) {
-		if (user === undefined || user.role !=='global_admin') {
+	if (user === undefined || user.role !=='global_admin') {
 			req.flash('auth', 'Not logged in!');
 			res.redirect('/login');
-		}
-		else {
-			// render user/add view
-		}
+	}
+	else {
+		// render add view. TODO: update this line when the view is implemented
+		res.render('users_template/user_page');
+	}
 });
 
 router.post('/add', function(req,res) {
 	var user = req.session.user;
-	  	if (user === undefined || user.role !=='global_admin') {
-		    	req.flash('auth', 'Not logged in!');
-	    		res.redirect('/login');
+	  if (user === undefined || user.role !=='global_admin') {
+			req.flash('auth', 'Not logged in!');
+	  	res.redirect('/login');
 		}
 		else {
 			// call db function for create user
 			// Parameters: (String) firstName, (String) lastName, (String) username,
 			// (String) password, (String) email, (Number) role
-			// TODO: verify password length and such
 			var firstName = req.body.firstName;
 			var lastName = req.body.lastName;
 			var username = req.body.username;
@@ -55,7 +55,14 @@ router.post('/add', function(req,res) {
 });
 
 router.get('/edit/:id', function(req, res) {
-
+	if (user === undefined || user.role !=='global_admin') {
+		req.flash('auth', 'Not logged in!');
+		res.redirect('/login');
+	}
+	else {
+		// render add view. TODO: update this line when the view is implemented
+		res.render('users_template/user_page');
+	}
 });
 
 router.post('/edit/:id', function(req, res) {
@@ -65,7 +72,13 @@ router.post('/edit/:id', function(req, res) {
 		res.redirect('/#login');
 	}
 	else {
-
+		// update email and password
+		db.updateEmail(req.body.username, req.body.email, function(error){
+			console.log(error);
+		});
+		db.updatePassword(req.body.username, req.body.password, function(error){
+			console.log(error);
+		});
 	}
 });
 
