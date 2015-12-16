45c487b5391e18ec84c99c02298ea4086538cb40
yongbin999 <yongbinliang@umass.edu>
2015-12-16 14:08:00 -0500

    add user function doesnt work

--- routes/webpage_routes.js
+++ routes/webpage_routes.js
@@ -45,7 +45,7 @@ router.post('/login', function (req, res) {
 
   db.getUser(post.user, function (data, userRole) {
    console.log("user data :"+ data);
-    if (data === undefined){
+    if (data === undefined || data == null){
       req.flash('auth', 'User does not exist');
       res.redirect('/#login');
     }
@@ -55,11 +55,13 @@ router.post('/login', function (req, res) {
     }
     else{
       //success
+      console.log(data)
     	req.session.user = data;
-  	req.session.user.role_description= userRole.getDescription();
+  	  req.session.user.role_description= userRole.getDescription();
     	res.render('control_panel', {userinfo:data});
 
 
+        res.redirect('/');
     }
   });
 
@@ -76,10 +78,33 @@ router.get('/logout', function(req,res) {
 // About page
 router.post('/signup', function(req, res) {
 	var user_obj = req.session.user;
-	res.render('about', { userinfo   : user_obj});
-	
-        res.redirect('/');
-
+  // call db function for create user
+      // Parameters: (String) firstName, (String) lastName, (String) username,
+      // (String) password, (String) email, (Number) role
+      var firstName = req.body.firstName;
+      var lastName = req.body.lastName;
+      var username = req.body.username;
+      var password = req.body.password;
+      if(password.length < 6) {
+        console.log("Password isn't long enough.");
+        //res.flash <-- do this, similar to above
+            req.flash('signup', 'Password isn\'t long enough.');
+        res.redirect('back');
+      }
+      var email = req.body.email;
+      //var role = req.body.role;
+        console.log("before")
+        db.createUser(firstName, lastName, username, password, email, role, function(err){
+          console.log("heey")
+          if(err){
+            console.log(err.getDescription);
+          }
+          else{
+            console.log(err);
+                req.flash('signup', 'create success');
+            res.redirect('/back');
+          }
+        });
 });
 
 
